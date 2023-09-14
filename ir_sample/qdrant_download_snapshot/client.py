637ad72d9821420d7e4e684b450e7cf03b57ec5b
wararaki <ky7.ott.w@gmail.com>
2023-09-14 22:01:47 +0900

    update

--- ir_sample/qdrant_download_snapshot/client.py
+++ ir_sample/qdrant_download_snapshot/client.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from typing import List
 
+import requests
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Batch, ScoredPoint, UpdateResult, VectorParams, SnapshotDescription
 
@@ -38,3 +40,12 @@ class SearchClient:
     def create_snapshot(self, collection_name: str) -> SnapshotDescription:
         response = self._client.create_snapshot(collection_name=collection_name)
         return response
+
+    def download_snapshot(self, collection_name: str, snapshot_name: str, filepath: Path) -> bool:
+        url = f"{self._client._client.rest_uri}/collections/{collection_name}/snapshots/{snapshot_name}"
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(filepath, "wb") as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        return True
--- ir_sample/qdrant_download_snapshot/main.py
+++ ir_sample/qdrant_download_snapshot/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from qdrant_client.models import VectorParams
 
 from builder import QueryBuilder
@@ -22,60 +24,22 @@ def main():
     client.insert(collection_name, points)
     print(f"data inserted: {len(points.ids)}")
 
-    # prefilter
     condition = SearchCondition(city="London")
     print(condition)
     query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
     response = client.search(collection_name, query)
     show(response)
-    
-    condition = SearchCondition(color="blue")
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
 
-    condition = SearchCondition(color="red", city="London")
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
+    print("create snapshot:")
+    response = client.create_snapshot(collection_name)
+    print(f"{response.name}")
+    print("")
 
-    condition = SearchCondition()
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
-
-    condition = SearchCondition(color="yellow", is_empty_city=True)
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
-
-    condition = SearchCondition(is_empty_city=True)
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
-
-    condition = SearchCondition(is_empty_color=True)
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
-
-    condition = SearchCondition(city="London", is_empty_city=True)
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
-
-    condition = SearchCondition(is_empty_color=True, is_empty_city=True)
-    print(condition)
-    query = QueryBuilder.build(condition=condition, vector=[0.9, 0.1, 0.1])
-    response = client.search(collection_name, query)
-    show(response)
+    print("download snaphost:")
+    download_path = Path("download/index.snapshot")
+    response = client.download_snapshot(collection_name, response.name, download_path)
+    print(f"{response}")
+    print()
 
     _ = client.delete_index(collection_name)
     print(f"index deleted: {collection_name}")
