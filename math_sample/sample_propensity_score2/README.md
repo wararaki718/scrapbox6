95f6119bdceb4193ac9f451a4c690d33f6b04940
wararaki <ky7.ott.w@gmail.com>
2023-10-19 23:32:44 +0900

    update

---
+++ math_sample/sample_propensity_score2/README.md
@@ -0,0 +1,13 @@
+# propensity score
+
+## setup
+
+```shell
+pip install numpy pandas statsmodels
+```
+
+## run
+
+```shell
+python main.py
+```
---
+++ math_sample/sample_propensity_score2/main.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+
+def main() -> None:
+    np.random.seed(0)
+    size = 120
+
+    X1 = np.random.uniform(0, 1, size)
+    X2 = np.random.choice([0, 1], p=[0.5, 0.5], size=size)
+
+    D_noise = np.random.uniform(0, 0.5, size)
+    D_threshold = 0.25 * X1 + 0.25 * X2 + D_noise
+    D = np.where(D_threshold >= 0.5, 1, 0)
+
+    y_noise = np.random.normal(0, 10, size)
+    y = np.clip(40 * X1 + 20 * X2 + 20 * D + y_noise, 0, 100).astype(int)
+
+    df = pd.DataFrame({
+        "motivation": X1,
+        "science": X2,
+        "special_course": D,
+        "score": y
+    })
+    print(df.head(3))
+    print()
+
+    y0 = df[df.special_course == 0].score
+    y1 = df[df.special_course == 1].score
+    print(y1.mean() - y0.mean())
+    print()
+
+    X = df[["motivation", "science"]]
+    X = sm.add_constant(X)
+
+    D = df.special_course
+    model = sm.Logit(D, X)
+    result = model.fit()
+    predicts = result.predict(X)
+    df["propensity_score"] = predicts
+    print(df.head(3))
+    print()
+
+    print("DONE")
+
+
+if __name__ == "__main__":
+    main()
