257fa07d0eadc21d622a27e65df3c9d567e3d3f8
wararaki <ky7.ott.w@gmail.com>
2024-01-29 21:47:33 +0900

    update

---
+++ rust_sample/catr/tests/cli.rs
@@ -0,0 +1,55 @@
+use assert_cmd::Command;
+use rand::{distributions::Alphanumeric, Rng};
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+const PRG: &str = "catr";
+const EMPTY: &str = "tests/inputs/empty.txt";
+const FOX: &str = "tests/inputs/fox.txt";
+const SPIDERS: &str = "tests/inputs/spiders.txt";
+const BUSTLE: &str = "tests/inputs/the-bustle.txt";
+
+
+fn gen_bad_file() -> String {
+    loop {
+        let filename: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+
+
+#[test]
+fn skips_bad_file() -> TestResult {
+    let bad = gen_bad_file();
+    let expected = format!("{}: .* [(]os error 2[)]", bad);
+    Command::cargo_bin(PRG)?
+        .arg(&bad)
+        .assert()
+        .success()
+        .stderr(predicates::str::is_match(expected)?);
+    Ok(())
+}
+
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin(PRG)?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn bustle() -> TestResult {
+    run(&[BUSTLE], "tests/expected/the-bustle.txt.out")
+}
