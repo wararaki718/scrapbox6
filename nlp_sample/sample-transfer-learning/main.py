76c5876dc77ffd41b46e6b348eca6d9f806fc6b6
wararaki <ky7.ott.w@gmail.com>
2024-04-18 23:11:12 +0900

    update

--- nlp_sample/sample-transfer-learning/main.py
+++ nlp_sample/sample-transfer-learning/main.py
@@ -1,4 +1,8 @@
+import pandas as pd
 from datasets import load_dataset
+
+from model import NNModel
+from vectorizer import TextVectorizer
 
 
 def main() -> None:
@@ -8,14 +12,28 @@ def main() -> None:
     print(f"valid: {len(dataset['validation'])}")
     print(f"test: {len(dataset['test'])}")
 
-    model_name = "google-bert/bert-base-cased"
-    print(type(dataset["train"]))
+    print(type(dataset['train']))
+    train_df = pd.DataFrame(dataset["train"])
+    print(train_df.shape)
 
-    cnt = 0
-    for x in dataset["train"]:
-        cnt += 1
-    print(cnt)
-    print(x)
+    model_name = "google-bert/bert-base-cased"
+    vectorizer = TextVectorizer(model_name=model_name)
+
+    X_train = []
+    chunksize = 128
+    for i in range(0, train_df.shape[0], chunksize):
+        texts = train_df.iloc[i: i+chunksize].text
+        #print(texts.shape)
+        x = texts.apply(vectorizer.transform)
+        break
+    print(x.shape)
+    print(len(x[0]))
+    print(x[:3])
+
+    n_input = 768
+    n_hidden = 128
+    n_output = 6
+    model = NNModel(n_input, n_hidden, n_output)
     print("DONE")
 
 
---
+++ nlp_sample/sample-transfer-learning/model.py
@@ -0,0 +1,25 @@
+import torch
+
+
+class NNModel(torch.nn.Module):
+    def __init__(
+        self,
+        n_input: int,
+        n_hidden: int,
+        n_output: int
+    ) -> None:
+        super().__init__()
+
+        layers = [
+            torch.nn.Linear(n_input, n_hidden),
+            torch.nn.Dropout(p=0.001),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(n_hidden, n_hidden),
+            torch.nn.Dropout(p=0.001),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(n_hidden, n_output),
+        ]
+        self._model = torch.nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._model(x)
