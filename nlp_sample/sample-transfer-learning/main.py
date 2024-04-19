ba294db5844ec4f22fda6a51226664407c22c732
wararaki <ky7.ott.w@gmail.com>
2024-04-19 23:22:57 +0900

    update

--- nlp_sample/sample-transfer-learning/main.py
+++ nlp_sample/sample-transfer-learning/main.py
@@ -1,8 +1,12 @@
+import gc
+
 import pandas as pd
 from datasets import load_dataset
+from tqdm import tqdm
 
 from model import NNModel
 from vectorizer import TextVectorizer
+from utils import try_gpu
 
 
 def main() -> None:
@@ -12,28 +16,27 @@ def main() -> None:
     print(f"valid: {len(dataset['validation'])}")
     print(f"test: {len(dataset['test'])}")
 
-    print(type(dataset['train']))
     train_df = pd.DataFrame(dataset["train"])
     print(train_df.shape)
 
     model_name = "google-bert/bert-base-cased"
     vectorizer = TextVectorizer(model_name=model_name)
-
     X_train = []
-    chunksize = 128
-    for i in range(0, train_df.shape[0], chunksize):
-        texts = train_df.iloc[i: i+chunksize].text
-        #print(texts.shape)
-        x = texts.apply(vectorizer.transform)
-        break
-    print(x.shape)
-    print(len(x[0]))
-    print(x[:3])
+    chunksize = 64
+    for i in tqdm(range(0, train_df.shape[0], chunksize)):
+        texts = train_df.iloc[i: i+chunksize].text.tolist()
+        x = vectorizer.transform(texts)
+        X_train.append(x)
+    print(len(X_train))
+    del vectorizer
+    gc.collect()
 
     n_input = 768
     n_hidden = 128
     n_output = 6
     model = NNModel(n_input, n_hidden, n_output)
+    model = try_gpu(model)
+    
     print("DONE")
 
 
--- nlp_sample/sample-transfer-learning/vectorizer.py
+++ nlp_sample/sample-transfer-learning/vectorizer.py
@@ -1,16 +1,29 @@
+from functools import partial
 from typing import List
 
 import torch
 from transformers import AutoModel, AutoTokenizer
+from transformers.tokenization_utils_base import BatchEncoding
+from utils import try_gpu
 
 
 class TextVectorizer:
     def __init__(self, model_name: str) -> None:
-        self._tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self._model = AutoModel.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, device_map="cuda")
+        self._tokenize = partial(
+            tokenizer.batch_encode_plus,
+            padding="max_length",
+            max_length=128,
+            truncation=True,
+            return_tensors="pt"
+        )
+        self._model = AutoModel.from_pretrained(model_name, device_map="cuda")
     
-    def transform(self, text: str) -> List[float]:
-        inputs: dict = self._tokenizer(text, return_tensors="pt")
+    def transform(self, texts: List[str]) -> torch.Tensor:
+        inputs: BatchEncoding = self._tokenize(texts)
+        inputs["input_ids"] = try_gpu(inputs["input_ids"])
+        inputs["token_type_ids"] = try_gpu(inputs["token_type_ids"])
+        inputs["attention_mask"] = try_gpu(inputs["attention_mask"])
         outputs = self._model(**inputs)
-        embeddings: torch.Tensor = outputs.last_hidden_state[0].mean(axis=0)
+        embeddings: torch.Tensor = outputs.last_hidden_state[:, 0, :]
         return embeddings.cpu().detach()
