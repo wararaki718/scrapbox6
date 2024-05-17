2c185d544902b8bad39d56d7bba9e9575055e548
wararaki <ky7.ott.w@gmail.com>
2024-05-17 21:51:27 +0900

    update

---
+++ rust_sample/linting-sample-code/README.md
@@ -0,0 +1,19 @@
+# linting
+
+## setup
+
+```shell
+rustup component add rustfmt clippy
+```
+
+## run
+
+linting
+
+```shell
+cargo clippy
+```
+
+```shell
+cargo fmt --all --check
+```
---
+++ rust_sample/linting-sample-code/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+        println!("hello, world");
+
+    let x = 3.1415;
+    let r = 8.0;
+    println!("the area of the circle is {}", x * r * r);
+
+    println!("DONE");
+}
