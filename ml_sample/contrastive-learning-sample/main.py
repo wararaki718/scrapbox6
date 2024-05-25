a53ec7f9d37b01291859acbbd38686d129c9bb9d
wararaki <ky7.ott.w@gmail.com>
2024-05-25 23:48:06 +0900

    update

--- ml_sample/contrastive-learning-sample/main.py
+++ ml_sample/contrastive-learning-sample/main.py
@@ -2,29 +2,31 @@ from datasets import load_dataset
 
 
 def main() -> None:
-    # dataset_name = "bclavie/mmarco-japanese-hard-negatives"
-    # data = load_dataset(dataset_name)
-    # print(data.shape)
-    # print(data.column_names)
-    # print(type(data))
-    # print(data)
-    # print(type(data["train"]["query"]))
-    # print(type(data["train"]["positives"]))
-    # print(type(data["train"]["negatives"]))
+    # use
+    dataset_name = "bclavie/mmarco-japanese-hard-negatives"
+    data = load_dataset(dataset_name)
+    print(data.shape)
+    print(data.column_names)
+    print(type(data))
+    print(data)
+    print(type(data["train"]["query"]))
+    print(type(data["train"]["positives"]))
+    print(type(data["train"]["negatives"]))
 
-    interaction_name = "unicamp-dl/mmarco"
-    interaction = load_dataset(interaction_name, "japanese")
-    print(interaction.shape)
-    print(interaction.column_names)
+    # TODO remove
+    # interaction_name = "unicamp-dl/mmarco"
+    # interaction = load_dataset(interaction_name, "japanese")
+    # print(interaction.shape)
+    # print(interaction.column_names)
 
-    print(interaction["train"]["query"][:3])
-    print()
+    # print(interaction["train"]["query"][:3])
+    # print()
 
-    print(interaction["train"]["positive"][:3])
-    print()
+    # print(interaction["train"]["positive"][:3])
+    # print()
 
-    print(interaction["train"]["negative"][:3])
-    print()
+    # print(interaction["train"]["negative"][:3])
+    # print()
 
     print("DONE")
 
